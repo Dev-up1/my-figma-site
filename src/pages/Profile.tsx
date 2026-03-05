d8b5b5bfb4cecd73afbe5d1f05be5ca3d2008103
Dev-up1 <electrosign0@gmail.com>
2026-03-05 03:05:59 +0300

    Update Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+
 export function Profile() {
   return (
-    <div style={{ padding: "50px", fontSize: "50px", color: "red", textAlign: "center" }}>
-      تست تست هل تسمعني؟
+    <div className="min-h-screen flex items-center justify-center bg-red-600">
+      <h1 className="text-white text-6xl font-bold">
+        تم مسح الصفحة
+      </h1>
     </div>
   );
 }
